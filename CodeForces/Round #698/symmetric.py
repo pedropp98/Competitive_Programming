6d1eb461aa273be5badfefecbfa5949b85e2801f
Pedro Perez <pedropc@usp.br>
2021-01-28 22:02:42 -0300

    Adding symetric.py file

---
+++ CodeForces/Round #698/symmetric.py
@@ -0,0 +1,10 @@
+def main():
+    test_cases = int(input())
+    for _ in range(test_cases):
+        n = int(input())
+        input_2n = input().split()
+        for i in range(2 * n):
+            
+
+if __name__ == '__main__':
+    main()
