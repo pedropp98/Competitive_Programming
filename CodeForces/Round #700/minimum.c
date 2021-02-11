4d51835aa49a13342a434c2d0f8d31c589907422
Pedro Perez <pedropc@usp.br>
2021-02-10 23:22:59 -0300

    Adding minimum.c file

---
+++ CodeForces/Round #700/minimum.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define not !
+#define and &&
+#define false 0
+#define true 1
+
+int request_value(int);
+int n;
+
+int main(void){
+    scanf("%d", &n);
+    int left_range = 1, right_range = n;
+    int *array = (int*)calloc((n+2), sizeof(int));
+    array[0] = array[n+1] = n + 1;
+ 
+    while(left_range < right_range){
+        int middle = (int) ((right_range+left_range) / 2);
+        if(request_value(middle))
+            scanf("%d", &array[middle]);
+        if(request_value(middle+1))
+            scanf("%d", &array[middle+1]);
+        if(array[middle] < array[middle+1])
+            right_range = middle;
+        else
+            left_range = middle+1;
+    }
+
+    printf("! %d\n", left_range);
+    fflush(stdout);
+    free(array);
+    return 0;
+}
+
+int request_value(int index){
+    if(index >= 1 and index <= n){
+        printf("? %d\n", index);
+        fflush(stdout);
+        return true;
+    }
+}
